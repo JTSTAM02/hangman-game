7b1d43d419bb85524975922299ab181802f645f1
Tyler Stamm <tyler.stamm1313@gmail.com>
2023-06-03 15:53:46 +0000

    Update files

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-let randomPhrase;
+var alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var alphabetElement = document.getElementById('demo');
+  
+    function getAlphabetVisual() {
+      for (let i = 0; i < alphabet.length; i++) {
+        var letter = document.createElement('span');
+        letter.textContent = alphabet[i];
+        alphabetElement.appendChild(letter);
+      }
+    }
+  
+    
+    getAlphabetVisual();
 
+
+  
